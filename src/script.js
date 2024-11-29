28006fcb5b7544d075208f25ef36e83474a63f45
vinicinbora <vinicius.rjnunes@gmail.com>
2024-11-28 22:28:07 -0300

    primeiro commit

---
+++ src/script.js
@@ -0,0 +1,41 @@
+const botoes = document.getElementById[ btn , btn01, btn02 ]; // Seleciona todos os botões
+const imageBox = document.getElementById('imageBox');
+const description = document.getElementById('description');
+
+
+
+btn.addEventListener('click', () => {
+    imageBox.style.backgroundImage = "url('img/bt01.png')"; // Troca a imagem de fundo
+    btn.classList.add('active'); // Ativa o botão 2
+    btn01.classList.remove('active');
+    btn02.classList.remove('active');
+    description.textContent = 'Massa de chocolate recheada com chocolate preto, chocolate branco e morangos.'; // Desativa o botão 1
+  });
+
+  btn01.addEventListener('click', () => {
+    imageBox.style.backgroundImage = "url('img/bt02.png')"; // Troca a imagem de fundo
+    btn01.classList.add('active'); // Ativa o botão 2
+    btn.classList.remove('active');
+    btn02.classList.remove('active');
+    description.textContent = 'Massa de chocolate recheada de Nutella e bownie.'; // Desativa o botão 1
+
+  });
+
+  btn02.addEventListener('click', () => {
+    imageBox.style.backgroundImage = "url('img/bt03.png')"; // Troca a imagem de fundo
+    btn02.classList.add('active'); // Ativa o botão 2
+    btn.classList.remove('active');
+    btn01.classList.remove('active');
+    description.textContent = 'Massa Branca, Chocolate ao leite, chocolate branco e Confete'; // Desativa o botão 1
+
+  });
+
+ 
+
+  //--------------------------------------------------------------------------------
+
+
+
+  //----------------------------------------------------------------------------
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+   
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'black': '#000000',
+      'fundo': '#171717',
+      'jaloul': '#0c6d3a',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
+
